f642eda10c6267c8d5422626465f586c6ff1d86d
marc.friedli <marc.friedli@he-arc.ch>
2017-05-30 20:09:13 +0200

    Correctif d'erreurs lié au fait que le dernier commit n'avait pas été testé. Suppression d'informations inutiles liées aux exceptions. Suppression de 'return None' inutiles. Modification de l'affichage de l'help. Retour en arrière : la soupe se récupère en deux fonctions et non une.

--- animebot/anime_functions.py
+++ animebot/anime_functions.py
@@ -8,15 +8,18 @@ from bs4 import BeautifulSoup
 WEB_SITE = 'http://neko-san.fr/'
 
 
-class AnimeNotFoundException(Exception):
+class AnimeNotFound(Exception):
     """Exception levée quant un anime qui n'existe pas est passé en paramètre"""
-    pass
 
 
-class AnimeLicenciedException(Exception):
+class AnimeLicencied(Exception):
     """Exception levée quand on tente d'accéder à un épisode d'un animé licendié"""
-    pass
 
+
+async def get_url(session, url):
+    """Return l'url de manière à ce qu'il soit utilisable par bs."""
+    async with session.get(url) as response:
+        return await response.text()
 
 async def make_soup(session, url):
     """Return la bs de l'URL passé."""
@@ -24,20 +27,18 @@ async def make_soup(session, url):
         text = await resp.read()
     return BeautifulSoup(text.decode("utf-8"), "html5lib")
 
-async def execute_request(requ, *param):
+async def execute_request(requ, *params):
     """
         Exécute la fonction correspondante à 'requ'
     """
     async with aiohttp.ClientSession() as session:
         try:
-            return await request[requ.lower()](session, param)
-        except AnimeNotFoundException:
+            return await request[requ.lower()](session, params)
+        except AnimeNotFound:
             print("L'anime est introuvable.")
-            return None
         except KeyError:
             print("Instruction inconnue. tapez 'help' pour la liste des instructions")
-            return None
-        except AnimeLicenciedException:
+        except AnimeLicencied:
             print("Cet animé est déjà licencié. Soutenez-le en l'achetant.")
 
 async def get_informations(session, anime_name):
@@ -45,6 +46,7 @@ async def get_informations(session, anime_name):
         Return les informations sur l'anime passé en argument
         En revanche, c'est pas très beau à voir...
     """
+    anime_name = anime_name[0]
     soup = await make_soup(session, WEB_SITE+'anime')
     for anime_link in soup.find_all('a'):
         for anime_title in anime_link.find_all(attrs='anime-titre'):
@@ -58,26 +60,28 @@ async def get_informations(session, anime_name):
                         studio = node.string
                 year = anime_link.find(attrs='annee').string
                 return f"Anime \"{anime_name}\" de {author} par {studio} en {year}.\nRésumé : \"{resume}\""
-    raise AnimeNotFoundException
+    raise AnimeNotFound
 
 async def get_resume(url, session):
     """Return le résumé de l'anime contenu dans l'URL."""
     soup = await make_soup(session, WEB_SITE + url)
     return soup.find(attrs="resume").string
 
-async def get_episode(session, anime_name,  num_episod):
+async def get_episode(session, params):
     """Return le lien de l'épisode choisi"""
-    return "Not implemented yet."
+    anime_name = params[0]
+    num_episod = params[1]
+    raise NotImplementedError
 
 async def get_anime_after_search(session, search):
     """Return une liste d'anime répondant à search"""
-    return "Not implemented yet."
+    raise NotImplementedError
 
 async def get_instructions(*t):
     """Return les différentes instructions du module ainsi que leur descripiton et comment les utiliser"""
-    return f"Pour avoir les informations sur un anime : info, nom_de_l_anime\n" \
-           f"Pour obtenir un épisode particulier : episod, nom_de_l_anime, num_episode\n" \
-           f"Pour effectuer une recherche : search, type_de_recherche(genre/auteur), valeurs_recherchées"
+    return f"""Pour avoir les informations sur un anime : info, nom_de_l_anime
+Pour obtenir un épisode particulier : episod, nom_de_l_anime, num_episode
+Pour effectuer une recherche : search, type_de_recherche(genre/auteur), valeurs_recherchées"""
 
 
 request = {
--- tests/test_instructions.py
+++ tests/test_instructions.py
@@ -1,6 +1,10 @@
+import sys
+import os
+sys.path.append(os.getcwd())
 from animebot import anime_functions
 import pytest
 import aiohttp
+
 
 @pytest.mark.asyncio
 async def test_if_help_command_work():
@@ -14,10 +18,24 @@ async def test_if_help_command_with_all_case_work():
 
 @pytest.mark.asyncio
 async def test_if_info_command_work():
+    execute_request_with_info_reply = await  anime_functions.execute_request('info', 'Higurashi no Naku Koro ni')
     async with aiohttp.ClientSession() as session:
-        assert await anime_functions.execute_request('info', 'Higurashi no Naku Koro ni') \
-               == \
-               await anime_functions.get_informations('Higurashi no Naku Koro ni', session)
+        get_information_reply = await anime_functions.get_informations(session, ['Higurashi no Naku Koro ni'])
+    assert execute_request_with_info_reply == get_information_reply
+
+@pytest.mark.asyncio
+async def test_if_espisod_command_work():
+    try:
+        await anime_functions.execute_request('episod', 'Elfen Lied', 4)
+    except NotImplementedError:
+        assert True
+
+@pytest.mark.asyncio
+async def test_if_search_command_work():
+    try:
+        assert not await anime_functions.execute_request('search', 'Horreur', 'Comédie')
+    except NotImplementedError:
+        assert True
 
 
 @pytest.mark.asyncio
